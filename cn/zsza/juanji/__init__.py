706b49d93543a9b2e1174d3982e3b104086e9444
zhangsong01 <zhangsong01@missfresh.cn>
2019-08-12 10:58:39 +0800

    添加zx.jpg

--- cn/zsza/juanji/__init__.py
+++ cn/zsza/juanji/__init__.py
@@ -6,9 +6,16 @@
 from scipy.misc import imread
 import pandas as pd
 import imageio
+from PIL import Image
+import numpy as np
 
 img = imageio.imread('2.png')
 # img = imread('2.png')
 print(img.shape)
 img_df = pd.DataFrame(img.reshape((-1, 2)))
 img_df.to_csv('2.csv')
+
+
+img1 = np.array(Image.open('zx.jpg'))
+print(img1.shape)  # 返回(10, 10, 3)
+print(img1)
