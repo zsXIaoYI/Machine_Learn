42fe457c530b84767a70bc670a8e5dd2fd356c02
zhangsong01 <zhangsong01@missfresh.cn>
2020-01-03 21:18:01 +0800

    添加一些code

--- cn/zsza/_np/_2.py
+++ cn/zsza/_np/_2.py
@@ -10,11 +10,13 @@ import numpy as np
 ar1 = np.array([[1, 2],
                 [3, 4]])
 
+print('arr1的类型:', ar1.dtype)
+
 print(ar1[:, 0])  # 返回第一列
 print(ar1[:, 0, np.newaxis])  # 返回两行一列的矩阵
 print(isinstance(ar1[:, 0, np.newaxis], np.ndarray))
 
-print('****************f分隔符****************')
+print('****************分隔符****************')
 
 ar2 = np.array([[11, 12],
                 [13, 14]])
@@ -32,3 +34,11 @@ print(ar.shape)  # 返回(3, )
 ar1_2 = ar[np.newaxis, :]  # 变成一行三列的矩阵
 print(ar1_2)
 print(ar.reshape((-1, 1)))
+
+arr2 = np.arange(9).reshape((3, 3))
+print('arr2:\n', arr2)
+
+# axis=0 每列求和
+print('arr2每行求和:', arr2.sum(axis=1))
+
+
