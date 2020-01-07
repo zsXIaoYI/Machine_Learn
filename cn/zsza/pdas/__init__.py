a45d4bbda7804d6d2c5d981cb0c10226ceec8cf4
zhangsong01 <zhangsong01@missfresh.cn>
2020-01-07 20:25:00 +0800

    删除一些code

--- cn/zsza/pdas/__init__.py
+++ cn/zsza/pdas/__init__.py
@@ -2,13 +2,3 @@
 # __author__ = 'zs'
 # 2019/4/18 下午11:01
 
-import pandas as pd
-
-data = {'Name': ['小红', '小黑', '小白'],
-        'City': ['北京', '东京', '夏威夷'],
-        'Age': [20, 22, 26]}
-
-data_frame = pd.DataFrame(data)
-print('data_frame:\n', data_frame)
-print('\n')
-print(data_frame[data_frame.City != '北京'])
